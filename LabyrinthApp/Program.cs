db035dc73ab949b438b8c3a6ea0f4b395e50986e
Aliaksandr Rykau <Aliaksandr_Rykau@epam.com>
2020-01-09 16:51:43 +0300

    Add Labyrinth solution and LabyrinthApp project.

---
+++ LabyrinthApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabyrinthApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
